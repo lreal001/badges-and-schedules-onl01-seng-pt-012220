d3404499e851eb2de784c19b34570167f750ce47
Lessllie Real <rlessllie@yahoo.com>
2020-01-24 07:01:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+rooms 1-7
+"Hello, _____! You'll be assigned to room _____!"
+
+
+
+
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end 
+
+
+def batch_badge_creator(array)
+  array = []
+  returns badge_maker
+  
+end 
+
+def assign_rooms
+  speakers = []
+  
+  
+end 
+
+def printer
+  
+  
+end 
