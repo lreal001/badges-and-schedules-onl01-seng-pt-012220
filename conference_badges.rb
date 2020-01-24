b0e096e7b14b789ebf2edd0a985199f13acefbef
Lessllie Real <rlessllie@yahoo.com>
2020-01-24 07:06:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,10 @@ def batch_badge_creator(array)
   
 end 
 
-#
+#assign_rooms that takes the list of speakers and assigns each #speaker to a room. Make sure that each room only has one #speaker.
+
+#You have rooms 1-7.
+#return a list of room assignments in the form of: "Hello, _____! #You'll be assigned to room _____!"
 
 
 def assign_rooms
