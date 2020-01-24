c41e0fed8f47363e3ca2896b60ac2161d25c5661
Lessllie Real <rlessllie@yahoo.com>
2020-01-24 07:17:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -35,6 +35,11 @@ def assign_rooms
   1-7
   counter = +1 
   
+ # a = [11,22,31,224,44].each.with_index { |val,index| puts "index: #{index} for #{val}" if val < 30}
+#index: 0 for 11
+#index: 1 for 22
+#=> [11, 22, 31, 224, 44]
+  
 end 
 
 
